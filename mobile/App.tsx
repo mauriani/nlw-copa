2809d3b0dbf5f369b49c53531adc9e8f0ec0305d
mauriani <maurianim@gmail.com>
2022-11-10 10:33:14 -0300

    algumas mofificações

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,6 +9,8 @@ import {
 import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/styles/theme";
 import { SignIn } from "./src/screens/SignIn";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { New } from "./src/screens/New";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,12 +21,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor={"transparent"}
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={"transparent"}
+          translucent
+        />
+        {fontsLoaded ? <New /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, useState } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -8,19 +13,57 @@ interface UserProps {
 export interface AuthContextDataProps {
   user: UserProps;
   signIn: () => Promise<void>;
+  isUserLoading: Boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
-export function AuthContextProvider({ children }) {
-  const [user, setUser] = useState({ name: "Mauriani", avatarUrl: "" });
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
 
-  async function signIn() {}
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "921913547861-jj621oosdhh189veav2m14o814hfnm6e.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
+  async function signIn() {
+    try {
+      setIsUserLoading(true);
+
+      await promptAsync();
+    } catch (err) {
+      console.log(err);
+      throw err;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("TOKEN", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user,
+        user: {
+          name: "Mauriani",
+          avatarUrl: "",
+        },
+        isUserLoading,
       }}
     >
       {children}
