a38cd7f0ecd9bdfa7faa0bef07771c9424081001
mauriani <maurianim@gmail.com>
2022-11-23 13:30:53 -0300

    aula 04

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,12 +3,15 @@ import { useAuth } from "../hooks/useAuth";
 import { AppRoutes } from "./app.routes";
 
 import { SignIn } from "../screens/SignIn";
+import { Box } from "native-base";
 
 export function Routes() {
   const { user } = useAuth();
   return (
-    <NavigationContainer>
-      {user.name ? <AppRoutes /> : <SignIn />}
-    </NavigationContainer>
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   );
 }
